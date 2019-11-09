d2c48038e92ff11e5731b6a15cf0e2ffd93b8a3a
nile16 <nils.leandersson@gmail.com>
2019-11-09 01:37:28 +0100

    Updated migrate.sql

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reports;
+
 CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(64) NOT NULL,
     birth VARCHAR(16) NOT NULL,
@@ -16,6 +19,9 @@ CREATE TABLE IF NOT EXISTS reports (
 
 INSERT INTO reports (week, report)
 VALUES
-    ( 1, "Test rapport1"),
-    ( 2, "Test rapport2"),
-    ( 3, "Test rapport3");
+    ( 1, "Test rapport 1"),
+    ( 2, "Test rapport 2"),
+    ( 3, "Test rapport 3"),
+    ( 4, "Test rapport 4"),
+    ( 5, "Test rapport 5"),
+    ( 6, "Test rapport 6");
