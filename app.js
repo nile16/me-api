5be9fc9179c7d64e2d76537bb7c5719b1649e8a5
nile16 <nils.leandersson@gmail.com>
2019-11-09 01:00:39 +0100

    Beta edition

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/texts.sqlite');
+//export JWT_SECRET='jfsdkjdgt7775jmcg678ncv67yvrn6v76yvm9x9xgckewlkwrgj845knjwgkljwe'
+
+const secret = process.env.JWT_SECRET;
+const saltRounds = 10;
+const myPlaintextPassword = 'longandhardP4$$w0rD';
 
 const app = express();
 const port = 1337;
+
+var jsonParser = bodyParser.json();
+
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+    // spara lösenord i databasen.
+});
+
+
 
 app.use(cors());
 
@@ -13,49 +31,143 @@ if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
 }
 
-// Add a route
+
 app.get("/", (req, res) => {
     const data = {
-        data: {
-            msg: "Hello World"
-        }
+        msg: "Detta API är en skapelse av Nils Leandersson"
     };
 
     res.json(data);
 });
 
-// Testing routes with method
-app.get("/user", (req, res) => {
-    res.json({
-        data: {
-            msg: "Got a GET request, sending back default 200"
+
+app.get("/reports/week:week", (req, res) => {
+    db.all("SELECT * FROM reports WHERE week=?",
+    req.params.week, (err, rows) => {
+        if (err) {
+            res.json(
+                {
+                    error: "SQL failure",
+                    details: err
+                }
+            );
+        } else if (rows[0]) {
+            res.json({
+                report: rows[0].report
+            })
+        } else {
+            res.json({
+                error: "Week does not exist"
+            })
         }
     });
 });
 
-app.post("/user", (req, res) => {
-    res.status(201).json({
-        data: {
-            msg: "Got a POST request, sending back 201 Created"
-        }
+
+app.post("/register", jsonParser, (req, res) => {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        db.run("INSERT INTO users (name, birth, email, password) VALUES (?, ?, ?, ?)",
+            req.body.name, req.body.birth, req.body.email, hash, (err) => {
+                if (err) {
+                    if (err.errno=19) {
+                        res.json(
+                            {
+                                error: "Email exist",
+                            }
+                        )
+                    } else {
+                    res.json(
+                        {
+                            error: "SQL failure",
+                            details: err
+                        }
+                    )}
+                } else {
+                    res.status(201).json(
+                        {
+                            msg: "User inserted"
+                        }
+                    )
+                }
+            }
+        );
     });
 });
 
-app.put("/user", (req, res) => {
-    // PUT requests should return 204 No Content
-    res.status(204).send();
+
+app.post("/login", jsonParser, (req, res) => {
+    db.all("SELECT * FROM users WHERE email=?",
+        req.body.email, (err, rows) => {
+            if (err) {
+                res.json(
+                    {
+                        error: "SQL failure",
+                        details: err
+                    }
+                );
+            } else if (rows[0]) {
+                bcrypt.compare(req.body.password, rows[0].password, function(err, valid) {
+                    if (valid) {
+                        const token = jwt.sign({email:req.body.email}, secret);
+                        res.json({
+                            error: false,
+                            token: token
+                        })
+                    } else {
+                        res.json({
+                            error: "Invalid password"
+                        })
+                    }
+                });
+            } else {
+                res.json({
+                    error: "User does not exist"
+                })
+            }
+        }
+    )
 });
 
-app.delete("/user", (req, res) => {
-    // DELETE requests should return 204 No Content
-    res.status(204).send();
+
+app.post("/reports", jsonParser, (req, res) => {
+    jwt.verify(req.body.token, process.env.JWT_SECRET, function(err, decoded) {
+        if (err) {
+            res.json(
+                {
+                    error: "JWT failure",
+                    details: err
+                }
+            );
+        } else {
+            db.run("REPLACE INTO reports (week, report) VALUES (?, ?)",
+                req.body.week, req.body.report, (err) => {
+                    if (err) {
+                        res.json(
+                            {
+                                error: "SQL failure",
+                                details: err
+                            }
+                        );
+                    } else {
+                        res.status(201).json(
+                            {
+                                msg: "Report inserted"
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    })
 });
+
 
 app.use((req, res, next) => {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
+
 
 app.use((err, req, res, next) => {
     if (res.headersSent) {
@@ -73,5 +185,6 @@ app.use((err, req, res, next) => {
     });
 });
 
+
 // Start up server
-app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+app.listen(port, () => console.log(`me-API listening on port ${port}!`));
--- db/migrate.sql
+++ db/migrate.sql
@@ -1,5 +1,21 @@
 CREATE TABLE IF NOT EXISTS users (
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(60) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    birth VARCHAR(16) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(128) NOT NULL,
     UNIQUE(email)
 );
+
+
+CREATE TABLE IF NOT EXISTS reports (
+    week INT NOT NULL,
+    report TEXT,
+    UNIQUE(week)
+);
+
+
+INSERT INTO reports (week, report)
+VALUES
+    ( 1, "Test rapport1"),
+    ( 2, "Test rapport2"),
+    ( 3, "Test rapport3");
